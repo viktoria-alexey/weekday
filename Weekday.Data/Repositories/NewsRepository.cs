b33c83cbcac7f7058cf0f05ab502f530826d59f7
Viktoria Alexey <v.aleksey@itransition.com>
2020-12-16 19:40:12 +0300

    Added UnitOfWork

---
+++ Weekday.Data/Repositories/NewsRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Weekday.Data.Interfaces;
+using Weekday.Data.Models;
+
+namespace Weekday.Data.Repositories
+{
+    public class NewsRepository : Repository<News>, INewsRepository
+    {
+        public NewsRepository(DbContext context) : base(context)
+        {
+        }
+
+        private ApplicationDbContext _appContext => (ApplicationDbContext)_context;
+
+
+        public async Task<List<News>> GetNewsAsync(string authorId, int page, int pageSize)
+        {
+            IQueryable<News> newsQuery = _context.Set<News>()
+                .OrderByDescending(x => x.CreateDate);
+
+            if (!string.IsNullOrEmpty(authorId))
+            {
+                newsQuery = newsQuery.Where(x => x.AuthorId == authorId);
+            }
+
+            if (page != -1)
+            {
+                newsQuery = newsQuery.Skip((page - 1) * pageSize);
+            }
+
+            if (pageSize != -1)
+            {
+                newsQuery = newsQuery.Take(pageSize);
+            }
+
+            return await newsQuery.ToListAsync();
+        }
+    }
+}
---
+++ Weekday.Data/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Weekday.Data.Interfaces;
+using Weekday.Data.Repositories;
+
+namespace Weekday.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        readonly ApplicationDbContext _context;
+
+        private INewsRepository _news;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public INewsRepository News
+        {
+            get
+            {
+                if (_news == null)
+                {
+                    _news = new NewsRepository(_context);
+                }
+
+                return _news;
+            }
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
