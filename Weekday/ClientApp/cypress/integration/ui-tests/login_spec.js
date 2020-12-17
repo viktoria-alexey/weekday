4dd08f6e4afd1837313821f2db26f704add57c5f
Nikolay Rovdo <Nikolay.Rovdo@sea.live>
2020-12-17 13:23:02 +0300

    remove cypress

--- Weekday/ClientApp/cypress/integration/ui-tests/login_spec.js
+++
@@ -1,8 +0,0 @@
-describe('Connect', () => {
-  it('Start page', () => {
-    cy.visit('https://localhost:44320/')
-	cy.contains('img');
-	cy.contains('.navbar-brand');
-	cy.contains('.nav-link');
-  })
-})
--- Weekday/ClientApp/cypress/integration/ui-tests/welcome_spec.js
+++
@@ -1,7 +0,0 @@
-describe('Connect', () => {
-  it('Start page', () => {
-    cy.visit('https://localhost:44320/')
-	cy.contains('Login').click()
-	cy.url().should('include', '/Identity/Account')
-  })
-})
